a823103535a168581b4c2911124465a40768499d
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2023-10-30 12:01:29 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,26 +1,20 @@
 /* Populate database with sample data. */
 
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (1, 'Agumon', '2020-02-03', 0, TRUE, 10.23);
-
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (2, 'Gabumon', '2018-11-15', 2, TRUE, 8.00);
-
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (3, 'Pikachu', '2021-01-07', 1, FALSE, 15.04);
-
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (4, 'Devimon', '2017-05-12', 5, TRUE, 11.00);
+INSERT INTO animals ( name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ( 'Agumon', '2020-02-03', 0, TRUE, 10.23),
+       ( 'Gabumon', '2018-11-15', 2, TRUE, 8.00),
+       ( 'Pikachu', '2021-01-07', 1, FALSE, 15.04),
+       ( 'Devimon', '2017-05-12', 5, TRUE, 11.00);
 /*-------------------------------------------------------------------------*/
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (5, 'Charmander', '2020-02-08', 0, FALSE, -11),
-       (6, 'Plantmon', '2021-11-15', 2, TRUE, -5.7),
-       (7, 'Squirtle', '1993-04-02', 3, FALSE, -12.13),
-       (8, 'Angemon', '2005-06-12', 1, TRUE, -45),
-       (9, 'Boarmon', '2005-06-07', 7, TRUE, 20.4),
-       (10, 'Blossom', '1998-10-13', 3, TRUE, 17),
-       (11, 'Ditto', '2022-05-14', 4, TRUE, 22);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Charmander', '2020-02-08', 0, FALSE, 11),
+       ( 'Plantmon', '2021-11-15', 2, TRUE, 5.7),
+       ( 'Squirtle', '1993-04-02', 3, FALSE,12.13),
+       ( 'Angemon', '2005-06-12', 1, TRUE, 45),
+       ( 'Boarmon', '2005-06-07', 7, TRUE, 20.4),
+       ( 'Blossom', '1998-10-13', 3, TRUE, 17),
+       ( 'Ditto', '2022-05-14', 4, TRUE, 22);
 
 /*----------------------INSERT data into owners---------------------------*/
 INSERT INTO owners (full_name, age)
@@ -99,7 +93,7 @@ VALUES
         Blossom visited Stephanie Mendez on May 24th, 2020.
         Blossom visited William Tatcher on Jan 11th, 2021.
 */
-INSERT INTO visits (vet_id, animal_id, visit_date)
+INSERT INTO visits (vet_id, animal_id, date_of_visit)
 VALUES
     ((SELECT id FROM vets WHERE name = 'William Tatcher'), (SELECT id FROM animals WHERE name = 'Agumon'), '2020-05-24'),
     ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM animals WHERE name = 'Agumon'), '2020-07-22'),
@@ -121,3 +115,13 @@ VALUES
     ((SELECT id FROM vets WHERE name = 'Maisy Smith'), (SELECT id FROM animals WHERE name = 'Boarmon'), '2020-08-03'),
     ((SELECT id FROM vets WHERE name = 'Stephanie Mendez'), (SELECT id FROM animals WHERE name = 'Blossom'), '2020-05-24'),
     ((SELECT id FROM vets WHERE name = 'William Tatcher'), (SELECT id FROM animals WHERE name = 'Blossom'), '2021-01-11');
+
+/*---------------------------------------------*/
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animal_id, vet_id, date_of_visit) 
+SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids,
+ generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
